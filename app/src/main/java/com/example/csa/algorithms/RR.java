e253795aa6491ddd0aee9e30a5bdba1fff786587
nightwolf021 <rezapoorbafrani021@gmail.com>
2020-06-25 00:01:07 +0430

    sjf & rr algorithm have been fixed :)

--- app/src/main/java/com/example/csa/algorithms/RR.java
+++ app/src/main/java/com/example/csa/algorithms/RR.java
@@ -21,99 +21,148 @@ public class RR extends parent_abs {
         output=set_data(input);
         Collections.sort(output,new parent_abs.SortbyAT());
 
-        while(true)
+        int i, total = 0, x, counter = 0, time_quantum=1;
+        int wait_time = 0, turnaround_time = 0;
+        //arrival_time[10], burst_time[10],
+        int[] temp=new int[num];
+        for(int j=0;j<num;j++)
         {
-            boolean flag=true;
-            for(int i=0 ;i<num;i++)
+            temp[j]=output.get(j).getCbt();
+        }
+        x=num;
+        for(total = 0, i = 0; x != 0;)
+        {
+            if(temp[i] <= time_quantum && temp[i] > 0)
             {
-                if(input.get(i).getArrival_time()<=t)
-                {
-                    if(input.get(i).getArrival_time()<=q)
-                    {
-                        if(input.get(i).getCbt()>0)
-                        {
-                            flag=false;
-                            if(input.get(i).getCbt()>q)
-                            {
-                                t+=q;
-                                input.get(i).setCbt(input.get(i).getCbt()-q);
-                                input.get(i).setArrival_time(input.get(i).getArrival_time()+q);
-                            }
-                            else
-                            {
-                                // last time
-                                t+=input.get(i).getCbt();
-                                output.get(i).setTurn_around_time(t- output.get(i).getArrival_time());
-                                output.get(i).setWaiting_time(t-output.get(i).getCbt()- output.get(i).getArrival_time());
-                                input.get(i).setCbt(0);
-                            }
-                        }
-                    }
-                    else if(input.get(i).getArrival_time()>q)
-                    {
-                        for(int j=0;j<num;j++)
-                    {
-                        if(input.get(j).getArrival_time() < input.get(i).getArrival_time())
-                        {
-                            if(input.get(j).getCbt()>0)
-                            {
-                                flag=false;
-                                if(input.get(j).getCbt()>q)
-                                {
-                                    t+=q;
-                                    input.get(j).setCbt(input.get(i).getCbt()-q);
-                                    input.get(j).setArrival_time(input.get(i).getArrival_time()+q);
-                                }
-                                else{
-                                    t+=input.get(j).getCbt();
-                                    output.get(j).setTurn_around_time(t- output.get(j).getArrival_time());
-                                    output.get(j).setWaiting_time(t-output.get(j).getCbt()- output.get(j).getArrival_time());
-                                    input.get(j).setCbt(0);
-                                }
-                            }
-                        }
-                    }
-
-                        if(input.get(i).getCbt()>0)
-                        {
-                            flag=false;
-
-                            if(input.get(i).getCbt()>q)
-                            {
-                                t+=q;
-                                input.get(i).setCbt(input.get(i).getCbt()-q);
-                                input.get(i).setArrival_time(input.get(i).getArrival_time()+q);
-                            }
-                            else
-                            {
-                                t+=input.get(i).getCbt();
-                                output.get(i).setTurn_around_time(t- output.get(i).getArrival_time());
-                                output.get(i).setWaiting_time(t-output.get(i).getCbt()- output.get(i).getArrival_time());
-                                input.get(i).setCbt(0);
-                            }
-
-                        }
-
-                    }
-                }
-                else if(input.get(i).getArrival_time()>t)
-                {
-                    t++;
-                    i--;
-                }
+                total = total + temp[i];
+                temp[i] = 0;
+                counter = 1;
             }
-            if(flag)
+            else if(temp[i] > 0)
             {
-                break;
+                temp[i] = temp[i] - time_quantum;
+                total = total + time_quantum;
+            }
+            if(temp[i] == 0 && counter == 1)
+            {
+                x--;
+                output.get(i).setWaiting_time(total - output.get(i).getArrival_time() - output.get(i).getCbt());
+                output.get(i).setTurn_around_time(total - output.get(i).getArrival_time());
+                output.get(i).setCompleted(total);
+                wait_time = wait_time + total - output.get(i).getArrival_time() - output.get(i).getCbt();
+                turnaround_time = turnaround_time + total - output.get(i).getArrival_time();
+                counter = 0;
+            }
+            if(i == num - 1)
+            {
+                i = 0;
+            }
+            else if( output.get(i+1).getArrival_time() <= total)
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
             }
         }
-        for (int i = 0; i < num; i++) {
-            average_waiting_time += output.get(i).getWaiting_time();
-            average_response_time += output.get(i).getTurn_around_time();
-        }
 
-        avg_output[0] = (float)average_waiting_time/num;
-        avg_output[1] = (float)average_response_time/num;
+        average_waiting_time = ((float)(wait_time * 1.0 / num));
+        average_response_time = ((float)(turnaround_time * 1.0 / num));
+
+//        while(true)
+//        {
+//            boolean flag=true;
+//            for(int i=0 ;i<num;i++)
+//            {
+//                if(input.get(i).getArrival_time()<=t)
+//                {
+//                    if(input.get(i).getArrival_time()<=q)
+//                    {
+//                        if(input.get(i).getCbt()>0)
+//                        {
+//                            flag=false;
+//                            if(input.get(i).getCbt()>q)
+//                            {
+//                                t+=q;
+//                                input.get(i).setCbt(input.get(i).getCbt()-q);
+//                                input.get(i).setArrival_time(input.get(i).getArrival_time()+q);
+//                            }
+//                            else
+//                            {
+//                                // last time
+//                                t+=input.get(i).getCbt();
+//                                output.get(i).setTurn_around_time(t- output.get(i).getArrival_time());
+//                                output.get(i).setWaiting_time(t-output.get(i).getCbt()- output.get(i).getArrival_time());
+//                                input.get(i).setCbt(0);
+//                            }
+//                        }
+//                    }
+//                    else if(input.get(i).getArrival_time()>q)
+//                    {
+//                        for(int j=0;j<num;j++)
+//                    {
+//                        if(input.get(j).getArrival_time() < input.get(i).getArrival_time())
+//                        {
+//                            if(input.get(j).getCbt()>0)
+//                            {
+//                                flag=false;
+//                                if(input.get(j).getCbt()>q)
+//                                {
+//                                    t+=q;
+//                                    input.get(j).setCbt(input.get(i).getCbt()-q);
+//                                    input.get(j).setArrival_time(input.get(i).getArrival_time()+q);
+//                                }
+//                                else{
+//                                    t+=input.get(j).getCbt();
+//                                    output.get(j).setTurn_around_time(t- output.get(j).getArrival_time());
+//                                    output.get(j).setWaiting_time(t-output.get(j).getCbt()- output.get(j).getArrival_time());
+//                                    input.get(j).setCbt(0);
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                        if(input.get(i).getCbt()>0)
+//                        {
+//                            flag=false;
+//
+//                            if(input.get(i).getCbt()>q)
+//                            {
+//                                t+=q;
+//                                input.get(i).setCbt(input.get(i).getCbt()-q);
+//                                input.get(i).setArrival_time(input.get(i).getArrival_time()+q);
+//                            }
+//                            else
+//                            {
+//                                t+=input.get(i).getCbt();
+//                                output.get(i).setTurn_around_time(t- output.get(i).getArrival_time());
+//                                output.get(i).setWaiting_time(t-output.get(i).getCbt()- output.get(i).getArrival_time());
+//                                input.get(i).setCbt(0);
+//                            }
+//
+//                        }
+//
+//                    }
+//                }
+//                else if(input.get(i).getArrival_time()>t)
+//                {
+//                    t++;
+//                    i--;
+//                }
+//            }
+//            if(flag)
+//            {
+//                break;
+//            }
+//        }
+//        for (int i = 0; i < num; i++) {
+//            average_waiting_time += output.get(i).getWaiting_time();
+//            average_response_time += output.get(i).getTurn_around_time();
+//        }
+
+        avg_output[0] = (float)average_waiting_time;
+        avg_output[1] = (float)average_response_time;
         out(output);
     }
 
--- app/src/main/java/com/example/csa/algorithms/sjf.java
+++ app/src/main/java/com/example/csa/algorithms/sjf.java
@@ -9,46 +9,68 @@ import java.util.Collections;
 
 public class sjf extends parent_abs {
 
-    public    float average_waiting_time= (float) 0.0;
-    public   float average_response_time= (float) 0.0;
-    public   static float[] avg_output= new float[2];
-    static ArrayList<output_process_model> output=new ArrayList<>();
+    public float average_waiting_time = (float) 0.0;
+    public float average_response_time = (float) 0.0;
+    public static float[] avg_output = new float[2];
+    static ArrayList<output_process_model> output = new ArrayList<>();
 
-    public static ArrayList<output_process_model> getOutput()
-    {
+    public static ArrayList<output_process_model> getOutput() {
         return output;
     }
 
-    public sjf(ArrayList<input_process_model> input)
-    {
-        int num=input.size();
-        output=set_data(input);
+    public sjf(ArrayList<input_process_model> input) {
+        int num = input.size(), temp = 0, val = -1;
+        int[] arr = new int[num];
+        output = set_data(input);
+        arr[0] = input.get(0).getArrival_time() + input.get(0).getCbt();
+        output.get(0).setTurn_around_time((int) (arr[0] - output.get(0).getArrival_time()));
+        output.get(0).setWaiting_time(output.get(0).getTurn_around_time() - output.get(0).getCbt());
+        output.get(0).setCompleted(arr[0]);
 
-        float prevEnd=0;
-        Collections.sort(output,new parent_abs.SortbyAT());
-        Collections.sort(output,new parent_abs.Sortbycbt());
-        Collections.sort(input,new parent_abs.SortbyAT_input());
-        Collections.sort(input,new parent_abs.Sortbycbt_input());
 
-        for(int i=0 ; i<num ;i++)
-        {
-            output.get(i).setCompleted(max(prevEnd,output.get(i).getArrival_time()) + output.get(i).getCbt());
-            output.get(i).setTurn_around_time((int)output.get(i).getCompleted()-output.get(i).getArrival_time());
-            output.get(i).setWaiting_time(output.get(i).getTurn_around_time()-output.get(i).getCbt());
-            prevEnd=output.get(i).getCompleted();
+        float prevEnd = 0;
+        Collections.sort(output, new parent_abs.SortbyAT());
+//        Collections.sort(output,new parent_abs.Sortbycbt());
+
+        for (int i = 0; i < num; i++) {
+            output.get(i).setCompleted(max(prevEnd, output.get(i).getArrival_time()) + output.get(i).getCbt());
+            output.get(i).setTurn_around_time((int) output.get(i).getCompleted() - output.get(i).getArrival_time());
+            output.get(i).setWaiting_time(output.get(i).getTurn_around_time() - output.get(i).getCbt());
+            prevEnd = output.get(i).getCompleted();
 
             average_waiting_time += output.get(i).getWaiting_time();
             average_response_time += output.get(i).getTurn_around_time();
 
         }
-        avg_output[0] = (float)average_waiting_time/num;
-        avg_output[1] = (float)average_response_time/num;
-       out(output);
+        for (int i = 1; i < num; i++) {
+            temp = arr[i - 1];
+            int low = output.get(i).getCbt();
+            val = i;
+            for (int j = i; j < num; j++) {
+                if (temp >= output.get(j).getArrival_time() && low >= output.get(j).getCbt()) {
+                    low = output.get(j).getCbt();
+                    val = j;
+                }
+            }
+
+            arr[val] = temp + output.get(val).getCbt();
+            output.get(val).setTurn_around_time(arr[val] - output.get(val).getArrival_time());
+            output.get(val).setWaiting_time(output.get(val).getTurn_around_time() - output.get(val).getCbt());
+           output.get(val).setCompleted(arr[val]);
+
+            Collections.swap(output,val,i);
+
+            arr[i] = arr[val];
+
+            avg_output[0] = (float) average_waiting_time / num;
+            avg_output[1] = (float) average_response_time / num;
+            out(output);
+        }
     }
 
-    public static float[] get_avg()
-    {
-        return avg_output;
+        public static float[] get_avg ()
+        {
+            return avg_output;
+        }
     }
 
-}
